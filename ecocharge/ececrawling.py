bdfbf4aae4ac788d2a0d6febf397152b36f0380f
wonjinseop <wonjinseop7@naver.com>
2024-06-19 14:25:25 +0900

    Docs: 메인페이지 자동차 리스트 크롤링  코드수정

--- ecocharge/ececrawling.py
+++ ecocharge/ececrawling.py
@@ -20,8 +20,8 @@ mycursor = mydb.cursor()
 
 # 셀레늄 사용 중 브라우저 꺼짐 현상 방지 옵션
 option = webdriver.ChromeOptions()
-# option.add_experimental_option('detach', False)
-option.add_argument('--headless')
+option.add_experimental_option('detach', True)
+# option.add_argument('--headless')
 
 # 크롬 드라이버를 별도로 설치하지 않고 버전에 맞는 드라이버를 사용하게 해 주는 코드
 service = webdriver.ChromeService(ChromeDriverManager().install())
@@ -41,13 +41,24 @@ div_list = soup.select('div._car_panel_wrapper > div.list_info')
 query = 'TRUNCATE TABLE tbl_crawling'
 mycursor.execute(query)
 
-img_list = div_list[0].select('div.thumb_area > a > img')
-name_list = div_list[0].select('div.info_area > strong.title > a._text')
-
-index = len(img_list)
+img_list = []
+for n in range(1, 9):
+    if n != 2:
+        if n != 6:
+            t.sleep(0.3)
+            driver.find_element(By.XPATH, '//*[@id="main_pack"]/div[6]/div[2]/div[1]/div/div[3]/div[1]/div[' + str(n) + ']/div[1]/a').click()
+            t.sleep(0.3)  
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            img = soup.select_one("#main_pack > div.sc_new.cs_common_module.case_normal.color_5._cs_car_single > div.cm_content_wrap._content > div:nth-child(1) > div > div.rel_aroundview._image_viewer_wrap > div.view_box.type_normal > div.img_area > a > img").attrs['src']
+            img_list.append(img)
+            t.sleep(0.3)
+            driver.back()
+    
+print(img_list)
+t.sleep(1)
 for n in range(0, 6):
     query = 'INSERT INTO tbl_crawling (img_url, info_url) VALUES(%s, %s)'
-    values = (img_list[n].attrs['src'], 'https://search.naver.com/search.naver' +  name_list[n].attrs['href'])
+    values = (img_list[n], 'https://search.naver.com/search.naver' +  div_list[0].select('div.info_area > strong.title > a._text')[n].attrs['href'])
     mycursor.execute(query, values)
 
 mydb.commit()
